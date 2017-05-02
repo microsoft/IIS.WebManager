74071a92b6e1dc968aa81a548d9e358b899d5153
Jimmy Campbell <jimmyca@microsoft.com>
2017-05-02 11:27:49 -0700

    Remove /// reference to prevent duplicate export errors.

--- app/main/main.ts
+++ app/main/main.ts
@@ -1,4 +1,3 @@
-/// <reference path="../../node_modules/@types/core-js/index.d.ts" />
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
