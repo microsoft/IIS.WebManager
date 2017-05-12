dbf8297a3adf7ae33246dcd33600d519d7bdf14c
Jimmy Campbell <jimmyca@microsoft.com>
2017-05-12 13:16:47 -0700

    Update api setup version.

--- js/settings.js
+++ js/settings.js
@@ -1,6 +1,6 @@
 ﻿var SETTINGS = {
     "version": "1.1.3",
-    "api_version": "1.1.0",
-    "api_setup_version": "1.1.0",
+    "api_version": "1.1.1",
+    "api_setup_version": "1.1.1",
     "api_download_url": "http://go.microsoft.com/fwlink/?LinkId=829373"
 }
