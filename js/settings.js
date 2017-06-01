77d085da38f36c642850972988f608b5b826567e
Jimmy Campbell <jimmyca@microsoft.com>
2017-06-01 12:47:46 -0700

    Bump UI version.

--- js/settings.js
+++ js/settings.js
@@ -1,5 +1,5 @@
 ﻿var SETTINGS = {
-    "version": "1.1.3",
+    "version": "1.1.4",
     "api_version": "1.1.1",
     "api_setup_version": "1.1.1",
     "api_download_url": "http://go.microsoft.com/fwlink/?LinkId=829373"
