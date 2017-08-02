8a187cff3b517a0cd3f4c2f40d033fb84b49545f
Jimmy Campbell <jimmyca@microsoft.com>
2017-08-02 10:59:22 -0700

    Update version.

--- js/settings.js
+++ js/settings.js
@@ -1,5 +1,5 @@
 ﻿var SETTINGS = {
-    "version": "2.0.0",
+    "version": "2.0.1",
     "api_version": "2.0.0",
     "api_setup_version": "2.0.0",
     "api_download_url": "http://go.microsoft.com/fwlink/?LinkId=829373"
