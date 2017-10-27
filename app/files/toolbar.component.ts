3453736aced184232b7b694a9c96339141c33c76
Jimmy Campbell <jimmyca@microsoft.com>
2017-10-27 16:53:28 -0700

    Added icons to differentiate between folders taht correspond to locations.

--- app/files/toolbar.component.ts
+++ app/files/toolbar.component.ts
@@ -8,8 +8,8 @@ import { FormsModule } from '@angular/forms';
         <div>
             <button class="delete" title="Delete" (click)="onDelete.next()" *ngIf="delete !== null" [attr.disabled]="delete === false || null"></button>
             <button title="Upload" (click)="onUpload.next()" *ngIf="upload !== null" [attr.disabled]="upload === false || null"><i class="fa fa-upload"></i></button>
-            <button title="New Folder" (click)="onNewFolder.next()" *ngIf="newFolder !== null" [attr.disabled]="newFolder === false || null"><i class="fa fa-folder manila"></i></button>
-            <button title="New Root" (click)="onNewLocation.next()" *ngIf="newLocation !== null" [attr.disabled]="newLocation === false || null"><i class="fa fa-folder manila"></i></button>
+            <button class="fi text-center directory" title="New Folder" (click)="onNewFolder.next()" *ngIf="newFolder !== null" [attr.disabled]="newFolder === false || null"><i></i></button>
+            <button class="fi text-center directory location" title="New Root" (click)="onNewLocation.next()" *ngIf="newLocation !== null" [attr.disabled]="newLocation === false || null"><i></i></button>
             <button title="New File" (click)="onNewFile.next()" *ngIf="newFile !== null" [attr.disabled]="newFile === false || null"><i class="fa fa-file-o"></i></button>
             <button class="refresh" title="Refresh" (click)="onRefresh.next()" *ngIf="refresh !== null" [attr.disabled]="refresh === false || null"></button>
         </div>
@@ -24,7 +24,10 @@ import { FormsModule } from '@angular/forms';
             border: none;
             float: right;
         }
-    `]
+    `],
+    styleUrls: [
+        'app/files/file-icons.css'
+    ]
 })
 export class ToolbarComponent {
     @Input() newLocation: boolean;
