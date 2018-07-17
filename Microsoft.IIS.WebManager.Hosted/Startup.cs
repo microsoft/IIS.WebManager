6efde272b3241fbd4523b06c8b4599d3fb86aafe
Peter Hsu <shhsu@microsoft.com>
2018-07-17 09:12:43 -0700

    Create dotnet core Hosted site

---
+++ Microsoft.IIS.WebManager.Hosted/Startup.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Rewrite;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microsoft.IIS.WebManager.Hosted
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            using (var iisUrlRewriteStreamReader = File.OpenText(Path.Join(env.WebRootPath, "web.config")))
+            {
+                var options = new RewriteOptions().AddIISUrlRewrite(iisUrlRewriteStreamReader);
+                app.UseRewriter(options);
+            }
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+        }
+    }
+}
