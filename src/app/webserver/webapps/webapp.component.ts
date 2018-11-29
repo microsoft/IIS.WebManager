05d72e62e3bd9e9eefb5218924e87c7c0ead58ab
Peter Hsu <shhsu@microsoft.com>
2018-11-29 12:50:38 -0800

    add styling back

--- src/app/webserver/webapps/webapp.component.ts
+++ src/app/webserver/webapps/webapp.component.ts
@@ -31,7 +31,15 @@ import {WebAppsService} from './webapps.service';
                 </item>
             </vtabs>
         </div>
-    `
+    `,
+    styles: [`
+        :host >>> .sidebar > vtabs .vtabs > .items {
+            top: 35px;
+        }
+        :host >>> .sidebar > vtabs .vtabs > .content {
+            top: 96px;
+        }
+    `]
 })
 export class WebAppComponent implements OnInit {
     id: string;
