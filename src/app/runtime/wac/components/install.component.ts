ce8dd75725fc01202ce160d11f8ed55b44db35f3
Peter Hsu <shhsu@microsoft.com>
2019-02-06 14:00:57 -0800

    Install page update

--- src/app/runtime/wac/components/install.component.ts
+++ src/app/runtime/wac/components/install.component.ts
@@ -9,36 +9,54 @@ const windowsPathValidationRegex = new RegExp('^(?:[a-z]:|\\\\\\\\[a-z0-9_.$●-
 
 @Component({
     template: `
-    <div class="center sme-focus-zone">
-        <div *ngIf='!_inProgress' style="min-width:700px">
-            <h4>Microsoft IIS Administration API is required to manage IIS server:</h4>
-            <p #apiPrompt>
-                <label>Microsoft IIS Administration API installation location</label>
-                <input class="form-control" type="text" [(ngModel)]="_adminAPILocation"/>
-            </p>
-            <p #dotnetPrompt>
-                <label>.NET Core Runtime install location (Optional)</label>
-                <input class="form-control" type="text" placeholder="IIS Administration API installer will fetch required .NET Core Runtime online" [(ngModel)]="_dotnetCoreLocation" />
-            </p>
+    <div class="padded sme-focus-zone">
+        <div *ngIf='!inProgress' style="min-width:700px">
+            <h3>Internet Information Service (IIS)</h3>
+            <span>To manage an IIS Server, you need to install the IIS Administration API</span>
+
+            <div>
+                <div><input type="radio" [(ngModel)]="useDefault" [value]="true" [checked]="useDefault">Install from Microsoft (internet connection required)</div>
+                <div>
+                    <input type="radio" [(ngModel)]="useDefault" [value]="false" [checked]="!useDefault">Install from specific location
+                    <div *ngIf="!useDefault">
+                        <p #apiPrompt>
+                            <label>API Installer location</label>
+                            <input class="form-control" type="text" [(ngModel)]="adminAPILocation"/>
+                        </p>
+                        <p #dotnetPrompt>
+                            <label>.NET Core Runtime installer location</label>
+                            <input class="form-control" type="text" [(ngModel)]="dotnetCoreLocation" />
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+
             <div *ngIf='userInputError'>
                 <p class="color-error">
                     {{userInputError}}
                 </p>
             </div>
             <p>
-                <button class="bttn background-active" (click)="install()">Install on {{_targetHost}}</button>
+                <button class="bttn background-active" (click)="install()">Install on {{targetHost}}</button>
             </p>
         </div>
-        <div *ngIf='_inProgress'>
+        <div *ngIf='inProgress'>
             <h1>Installing...</h1>
             <p><i class="fa fa-spinner fa-pulse fa-3x"></i></p>
-            <p><small class='block color-active'>{{_status}}</small></p>
+            <p><small class='block color-active'>{{status}}</small></p>
         </div>
     </div>
 `,
     styles: [`
-    .center {
-        text-align: center;
+    .padded {
+        text-align: left;
+        padding-left: 100px;
+        padding-top: 100px;
+    }
+
+    h3 {
+        font-weight: bold;
     }
 
     h1 {
@@ -80,35 +98,39 @@ const windowsPathValidationRegex = new RegExp('^(?:[a-z]:|\\\\\\\\[a-z0-9_.$●-
 `],
 })
 export class InstallComponent {
-    private _inProgress: boolean
-    private _targetHost: string
-    private _adminAPILocation: string
-    private _dotnetCoreLocation: string
-    @ViewChild('apiPrompt') apiPrompt: ElementRef
-    @ViewChild('dotnetPrompt') dotnetPrompt: ElementRef
-    userInputError: string
+    useDefault: boolean = true;
+    inProgress: boolean;
+    targetHost: string;
+    adminAPILocation: string;
+    dotnetCoreLocation: string;
+    userInputError: string;
 
+    @ViewChild('apiPrompt') apiPrompt: ElementRef;
+    @ViewChild('dotnetPrompt') dotnetPrompt: ElementRef;
     constructor(
         private router: Router,
         private appContext: AppContextService,
         @Inject("Runtime") private runtime: WACRuntime,
     ) {
-        this._adminAPILocation = SETTINGS.api_download_url
-        this._targetHost = this.appContext.activeConnection.nodeName
+        this.targetHost = this.appContext.activeConnection.nodeName;
     }
 
     clearWarnings(event: Event) {
-        (<HTMLElement> event.target).classList.remove('background-warning')
+        (<HTMLElement> event.target).classList.remove('background-warning');
+    }
+
+    onSelectionChanged(event) {
+        console.log(event)
     }
 
     private verifyLocation(fieldName: string, location: string, allowEmpty: boolean): string {
         if (location) {
             if (!urlValidationRegex.test(location) && !windowsPathValidationRegex.test(location)) {
-                return `Invalid ${fieldName}: ${location}`
+                return `Invalid ${fieldName}: ${location}`;
             }
         } else {
             if (!allowEmpty) {
-                return `${fieldName} cannot be empty`
+                return `${fieldName} cannot be empty`;
             }
         }
 
@@ -119,41 +141,44 @@ export class InstallComponent {
         }
     }
 
-    private verifyLocationPrompt(prompt: ElementRef, allowEmpty: boolean): boolean {
-        let fieldName = prompt.nativeElement.querySelector('label').textContent
-        let textBox = prompt.nativeElement.querySelector('input')
-        textBox.classList.remove('background-warning')
-        this.userInputError = this.verifyLocation(fieldName, textBox.value, allowEmpty)
-        let valid = this.userInputError == null
+    private verifyLocationPrompt(prompt: ElementRef): boolean {
+        let fieldName = prompt.nativeElement.querySelector('label').textContent;
+        let textBox = prompt.nativeElement.querySelector('input');
+        textBox.classList.remove('background-warning');
+        this.userInputError = this.verifyLocation(fieldName, textBox.value, false);
+        let valid = this.userInputError == null;
         if (!valid) {
-            textBox.classList.add('background-warning')
-            textBox.focus()
+            textBox.classList.add('background-warning');
+            textBox.focus();
         }
-        return valid
+        return valid;
     }
 
     public install() {
-        if (this.verifyLocationPrompt(this.apiPrompt, false)
-            && this.verifyLocationPrompt(this.dotnetPrompt, true)) {
-            this._inProgress = true
-            let args: any = {
-                command: 'install',
-                adminAPILocation: this._adminAPILocation,
+        var args: any = {
+            command: 'install',
+        };
+
+        if (this.useDefault) {
+            args.adminAPILocation = SETTINGS.api_download_url
+        } else {
+            if (this.verifyLocationPrompt(this.apiPrompt) && this.verifyLocationPrompt(this.dotnetPrompt)) {
+                args.adminAPILocation = this.adminAPILocation;
+                args.dotnetCoreLocation = this.dotnetCoreLocation;
             }
-            if (this._dotnetCoreLocation) {
-                args.dotnetCoreLocation = this._dotnetCoreLocation
-            }
-            var sub = this.runtime.PrepareIISHost(args).subscribe(_ => {}, e => {
-                let reason = 'unknown'
-                if (e.response && e.response.exception) {
-                    reason = e.response.exception
-                }
-                this.userInputError = `Installation failed. Error: ${reason}`
-                this._inProgress = false
-            }, () => {
-                this.router.navigate(['/'])
-                sub.unsubscribe()
-            })
         }
+        
+        this.inProgress = true;
+        var sub = this.runtime.PrepareIISHost(args).subscribe(_ => {}, e => {
+            let reason = 'unknown';
+            if (e.response && e.response.exception) {
+                reason = e.response.exception;
+            }
+            this.userInputError = `Installation failed. Error: ${reason}`;
+            this.inProgress = false;
+        }, () => {
+            this.router.navigate(['/']);
+            sub.unsubscribe();
+        });
     }
 }
