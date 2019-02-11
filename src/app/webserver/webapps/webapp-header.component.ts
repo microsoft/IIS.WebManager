7b1c60d074a74cbbcc5418202a24d7127b122284
Peter Hsu <shhsu@microsoft.com>
2019-02-11 14:01:40 -0800

    Moving start/stop/restart button (#312)

--- src/app/webserver/webapps/webapp-header.component.ts
+++ src/app/webserver/webapps/webapp-header.component.ts
@@ -1,36 +1,16 @@
-import { Component, Input, Inject, ViewChild } from '@angular/core';
-import { Router } from '@angular/router';
-
-import { Selector } from '../../common/selector';
+import { Component, Input } from '@angular/core';
 import { WebApp } from './webapp'
-import { WebAppsService } from './webapps.service'
-import { WebSitesService } from '../websites/websites.service';
 
 @Component({
     selector: 'webapp-header',
     template: `
         <div class="feature-header">
-            <div class="actions">
-                <div class="selector-wrapper">
-                    <button title="Actions" (click)="_selector.toggle()" [class.background-active]="(_selector && _selector.opened) || false"><i class="fa fa-caret-down"></i></button>
-                    <selector [right]="true">
-                        <ul>
-                            <li><a class="bttn link" title="Browse" [attr.title]="url" [attr.href]="url" target="_blank">Browse</a></li>
-                            <li><button class="delete" title="Delete" (click)="onDelete()">Delete</button></li>
-                        </ul>
-                    </selector>
-                </div>
-            </div>
             <div class="feature-title sme-focus-zone">
                 <h1 [title]="model.website.name + model.path">{{model.path}}</h1>
             </div>
         </div>
     `,
     styles: [`
-        .selector-wrapper {
-            position: relative;
-        }
-
         .feature-title h1:before {
             content: "\\f121";
         }
@@ -38,28 +18,4 @@ import { WebSitesService } from '../websites/websites.service';
 })
 export class WebAppHeaderComponent {
     @Input() model: WebApp;
-    @ViewChild(Selector) private _selector: Selector;
-
-    constructor( @Inject("WebAppsService") private _service: WebAppsService,
-        @Inject("WebSitesService") private _siteService: WebSitesService,
-        private _router: Router) {
-    }
-
-    onDelete() {
-        if (confirm("Are you sure you would like to delete '" + this.model.path + "'?")) {
-            this._service.delete(this.model)
-                .then(() => {
-                    this._router.navigate(['/webserver/websites/', { id: this.model.website.id }]);
-                });
-        }
-        this._selector.close();
-    }
-
-    private get url() {
-        if (!this.model.website || this.model.website.bindings.length == 0) {
-            return "";
-        }
-
-        return this._siteService.getUrl(this.model.website.bindings[0]) + this.model.path;
-    }
 }
--- src/app/webserver/websites/website-header.component.ts
+++ src/app/webserver/websites/website-header.component.ts
@@ -1,9 +1,4 @@
-import { Component, Input, Inject, ViewChild } from '@angular/core';
-import { RouterLink, Router } from '@angular/router';
-
-import { NotificationService } from '../../notification/notification.service';
-import { Selector } from '../../common/selector';
-import { WebSitesService } from './websites.service';
+import { Component, Input } from '@angular/core';
 import { WebSite } from './site';
 
 
@@ -11,20 +6,6 @@ import { WebSite } from './site';
     selector: 'website-header',
     template: `
         <div class="feature-header" *ngIf="site">
-            <div class="actions">
-                <div class="selector-wrapper">
-                    <button title="Actions" (click)="_selector.toggle()" [class.background-active]="(_selector && _selector.opened) || false"><i class="fa fa-caret-down"></i></button>
-                    <selector [right]="true">
-                        <ul>
-                            <li><a class="bttn link" title="Browse" [attr.title]="url" [attr.href]="url" target="_blank">Browse</a></li>
-                            <li><button class="start" title="Start" [attr.disabled]="site.status == 'started' ? true : null" (click)="onStart()">Start</button></li>
-                            <li><button class="stop" title="Stop" [attr.disabled]="site.status == 'stopped' ? true : null" (click)="onStop()">Stop</button></li>
-                            <li><button class="delete" title="Delete" (click)="onDelete()">Delete</button></li>
-                        </ul>
-                    </selector>
-                </div>
-            </div>
-            
             <div class="feature-title sme-focus-zone">
                 <h1 [ngClass]="site.status">{{site.name}}</h1>
                 <span class="status" *ngIf="site.status == 'stopped'">{{site.status}}</span>
@@ -32,10 +13,6 @@ import { WebSite } from './site';
         </div>
     `,
     styles: [`
-        .selector-wrapper {
-            position: relative;
-        }
-
         .feature-title h1:before {
             content: "\\f0ac";
         }
@@ -48,41 +25,4 @@ import { WebSite } from './site';
 })
 export class WebSiteHeaderComponent {
     @Input() site: WebSite;
-    @ViewChild(Selector) private _selector: Selector;
-
-    constructor(@Inject("WebSitesService") private _service: WebSitesService,
-        private _router: Router,
-        private _notificationService: NotificationService) {
-    }
-
-    onStart() {
-        this._service.start(this.site);
-        this._selector.close();
-    }
-
-    onStop() {
-        this._service.stop(this.site);
-        this._selector.close();
-    }
-
-    onDelete() {
-        this._notificationService.confirm("Delete Web Site", "Are you sure you would like to delete '" + this.site.name + "'?")
-            .then(confirmed => {
-                if (confirmed) {
-                    this._service.delete(this.site)
-                        .then(() => {
-                            this._router.navigate(["/webserver/web-sites"]);
-                        });
-                }
-                this._selector.close();
-            });
-    }
-
-    private get url() {
-        if (this.site.bindings.length == 0) {
-            return "";
-        }
-
-        return this._service.getUrl(this.site.bindings[0]);
-    }
 }
