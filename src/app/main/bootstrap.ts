b1ec46f67db292c7701d596ede8446f8020c1106
Peter Hsu <shhsu@microsoft.com>
2019-04-09 13:40:10 -0700

    code cleanup for debugging

---
+++ src/app/main/bootstrap.ts
@@ -0,0 +1,3 @@
+export function preload(): Promise<void> {
+    return Promise.resolve();
+}
---
+++ src/app/main/bootstrap.wac.ts
@@ -0,0 +1,20 @@
+import { CoreEnvironment } from "@microsoft/windows-admin-center-sdk/core";
+import { PowerShellScripts } from "generated/powershell-scripts";
+import { IsProduction } from "environments/environment";
+
+export function preload(): Promise<void> {
+    // initialize SME module environment with localization settings.
+    return CoreEnvironment.initialize(
+        {
+            name: "microsoft.iis",
+            powerShellModuleName: PowerShellScripts.module,
+            isProduction: IsProduction,
+            shellOrigin: '*'
+        },
+        {
+            resourcesPath: 'assets/strings',
+        },
+        {
+            disableStyleInjection: true,
+        })
+}
