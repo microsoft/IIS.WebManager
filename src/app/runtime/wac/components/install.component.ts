f16312d3006975c5391fe5e0c1942549e071ddf4
Peter Hsu <shhsu@microsoft.com>
2019-06-11 16:58:22 -0700

    checkpoint for installer

--- src/app/runtime/wac/components/install.component.ts
+++ src/app/runtime/wac/components/install.component.ts
@@ -1,11 +1,23 @@
 import { Component, Inject, ViewChild, ElementRef, OnInit } from "@angular/core";
-import { AppContextService } from "@microsoft/windows-admin-center-sdk/angular";
+import { AppContextService, FileTransferService } from "@microsoft/windows-admin-center-sdk/angular";
 import { SETTINGS } from "main/settings";
 import { Router, ActivatedRoute } from "@angular/router";
 import { WACRuntime } from "runtime/runtime.wac";
+import { Logger, LoggerFactory, LogLevel } from "diagnostics/logger";
+import { Observable, of } from "rxjs";
 
 const urlValidationRegex = new RegExp('^((http[s]?|ftp):\\/)?\\/?([^:\\/\\s]+)((\\/\\w+)*\\/)([\\w\\-\\.]+[^#?\\s]+)(.*)?(#[\\w\\-]+)?$', "i");
 const windowsPathValidationRegex = new RegExp('^(?:[a-z]:|\\\\\\\\[a-z0-9_.$●-]+(\\\\[a-z]\\$)?)\\\\(?:[^\\\\/:*?"<>|\\r\\n]+\\\\)*[^\\\\/:*?"<>|\\r\\n]*$', "i");
+
+const APILocationID = "apiLocation"
+const DotNetLocationID = "dotnetLocation"
+const AspNetLocationID = "aspnetLocation"
+
+enum LocationType {
+    Url = 1,
+    Local,
+    UNC,
+}
 
 @Component({
     template: `
@@ -14,24 +26,26 @@ const windowsPathValidationRegex = new RegExp('^(?:[a-z]:|\\\\\\\\[a-z0-9_.$●-
         <h3>Internet Information Service (IIS)</h3>
         <span>{{details}}</span>
         <ul class="form">
-            <li><input type="radio" [(ngModel)]="useDefault" [value]="true" [checked]="useDefault">Install from Microsoft (internet connection required)</li>
+            <li><input type="radio" [(ngModel)]="useDefault" [value]="true" [checked]="useDefault">Install from Microsoft</li>
             <li>
                 <input type="radio" [(ngModel)]="useDefault" [value]="false" [checked]="!useDefault">Install from specific location
                 <ul *ngIf="!useDefault">
                     <li>
                         <ul>
-                            <li #apiPrompt>
+                            <li #apiPrompt id="${APILocationID}">
                                 <label>API Installer location</label>
                                 <input class="form-control" type="text" [(ngModel)]="adminAPILocation"/>
                             </li>
-                            <li #dotnetPrompt>
+                            <li #dotnetPrompt id="${DotNetLocationID}">
                                 <label>.NET Core Runtime installer location, required version: ${SETTINGS.DotnetFrameworkVersion}</label>
                                 <input class="form-control" type="text" [(ngModel)]="dotnetCoreLocation" />
                             </li>
-                            <li #aspnetPrompt>
+                            <li #aspnetPrompt id="${AspNetLocationID}">
                                 <label>ASP.NET Core installer location, required version: ${SETTINGS.AspnetSharedFrameworkVersion}</label>
                                 <input class="form-control" type="text" [(ngModel)]="aspnetCoreLocation" />
                             </li>
+                            <input class="toggle-input" type="checkbox"  [(ngModel)]="transfer" />
+                            <label>download installers from local and then upload to {{targetHost}}</label>
                         </ul>
                     </li>
                 </ul>
@@ -114,6 +128,11 @@ const windowsPathValidationRegex = new RegExp('^(?:[a-z]:|\\\\\\\\[a-z0-9_.$●-
     .form-control {
         width: 100ch;
     }
+
+    label {
+        font-weight: normal;
+        margin: 0.3em;
+    }
 `],
 })
 export class InstallComponent implements OnInit {
@@ -125,17 +144,23 @@ export class InstallComponent implements OnInit {
     aspnetCoreLocation: string;
     userInputError: string;
     details: string;
+    logger: Logger;
+    transfer: boolean;
+    locationType: Map<string, LocationType> = new Map<string, LocationType | null>();
 
     @ViewChild('apiPrompt') apiPrompt: ElementRef;
     @ViewChild('dotnetPrompt') dotnetPrompt: ElementRef;
     @ViewChild('aspnetPrompt') aspnetPrompt: ElementRef;
 
     constructor(
+        factory: LoggerFactory,
         private route: ActivatedRoute,
         private router: Router,
         private appContext: AppContextService,
+        private transferService: FileTransferService,
         @Inject("Runtime") private runtime: WACRuntime,
     ) {
+        this.logger = factory.Create(this);
         this.targetHost = this.appContext.activeConnection.nodeName;
     }
 
@@ -153,35 +178,87 @@ export class InstallComponent implements OnInit {
         console.log(event);
     }
 
-    private verifyLocation(fieldName: string, location: string, allowEmpty: boolean): string {
-        if (location) {
-            if (!urlValidationRegex.test(location) && !windowsPathValidationRegex.test(location)) {
-                return `Invalid ${fieldName}: ${location}`;
-            }
-        } else {
-            if (!allowEmpty) {
+    private verifyLocation(fieldId: string, fieldName: string, location: string, allowEmpty: boolean): string {
+        this.locationType[fieldId] = null;
+        if (!location) {
+            if (allowEmpty) {
+                return null;
+            } else {
                 return `${fieldName} cannot be empty`;
             }
         }
 
+        var locationType: LocationType | null = null;
+        if (urlValidationRegex.test(location)) {
+            locationType = LocationType.Url;
+        }
+        if (windowsPathValidationRegex.test(location)) {
+            if (location.startsWith(`\\\\`)) {
+                locationType = LocationType.UNC;
+            } else {
+                locationType = LocationType.Local;
+            }
+        }
+
+        if (locationType == null) {
+            return `Invalid ${fieldName}: ${location}`;
+        }
+
         // NOTE: This code is injected here to prevent user using shared drive because it would be considered double hop with WinRM and wouldn't work
         // This needs to be revisited when Windows Admin Center completed their work on CredSSP
-        if (location && location.startsWith(`\\\\`)) {
+        if (!this.transfer && locationType == LocationType.UNC) {
             return `Currently installation from shared drive is not supported, the issue is being tracked on https://github.com/Microsoft/IIS.WebManager/issues/239`;
         }
+        this.locationType[fieldId] = locationType;
     }
 
     private verifyLocationPrompt(prompt: ElementRef): boolean {
+        let fieldId = prompt.nativeElement.id;
         let fieldName = prompt.nativeElement.querySelector('label').textContent;
         let textBox = prompt.nativeElement.querySelector('input');
         textBox.classList.remove('background-warning');
-        this.userInputError = this.verifyLocation(fieldName, textBox.value, false);
+        this.userInputError = this.verifyLocation(fieldId, fieldName, textBox.value, false);
         let valid = this.userInputError == null;
         if (!valid) {
             textBox.classList.add('background-warning');
             textBox.focus();
         }
         return valid;
+    }
+
+    private EnsureFile(filename: string, location: string, locationType: LocationType | null): Observable<string> {
+        if (!this.transfer) {
+            return of(location);
+        }
+
+        if (locationType == null) {
+            // Should be impossible to reach
+            throw `Location Type cannot be null`;
+        }
+
+        let locType = locationType as LocationType;
+
+        if (locType == LocationType.Local) {
+            return of(location);
+        }
+
+        let tempLocation = os.tmpdir();
+
+    }
+
+    private EnsureFiles(args: any): Observable<any> {
+        if (args.adminAPILocation) {
+
+        }
+
+        if (args.dotnetCoreLocation) {
+
+        }
+
+        if (args.aspnetSharedLocation) {
+
+        }
+        return new Observable<any>();
     }
 
     public install() {
@@ -196,14 +273,21 @@ export class InstallComponent implements OnInit {
                 args.adminAPILocation = this.adminAPILocation;
                 args.dotnetCoreLocation = this.dotnetCoreLocation;
                 args.aspnetCoreLocation = this.aspnetCoreLocation;
+            } else {
+                return;
             }
         }
         
         this.inProgress = true;
-        this.runtime.PrepareIISHost(args).subscribe(_ => {}, e => {
+
+        this.EnsureFiles(args).pipe(
+            this.runtime.PrepareIISHost
+        ).subscribe(_ => {}, e => {
             let reason = 'unknown';
             if (e.response && e.response.exception) {
                 reason = e.response.exception;
+            } else {
+                this.logger.log(LogLevel.WARN, `Error during installation ${e}`);
             }
             this.userInputError = `Installation failed. Error: ${reason}`;
             this.inProgress = false;
