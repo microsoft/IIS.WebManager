6f0327161ac8e86917fcfb9ae061244d1bff06c5
Peter Hsu <shhsu@microsoft.com>
2019-08-15 11:38:05 -0700

    testing signing

---
+++ signing/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace signing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            throw new Exception("This project is used for package signing only");
+        }
+    }
+}
